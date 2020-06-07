8eecf3e3b25faf20b9981da71832baf118a7f877
Pragadhesh G <gpragadhesh@gpragadhesh-a01.vmware.com>
2020-06-07 15:13:07 +0530

    Commit after checking working status

--- app/src/main/java/com/example/sbdemo/Credentials1.java
+++ app/src/main/java/com/example/sbdemo/Credentials1.java
@@ -3,6 +3,8 @@ package com.example.sbdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -22,9 +24,13 @@ public class Credentials1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credentials1);
         toolbar_cred1 = (Toolbar) findViewById(R.id.toolbar_credentials1);
-        setSupportActionBar(toolbar_cred1);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar_cred1.setNavigationIcon(R.drawable.ic_back);
+        toolbar_cred1.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openhome();
+            }
+        });
         designation_spinner = findViewById(R.id.Designationspinner);
         String [] designations = {"Junior","Senior","SuperSenior"};
         ArrayList<String> designation = new ArrayList<>(Arrays.asList(designations));
@@ -34,6 +40,11 @@ public class Credentials1 extends AppCompatActivity {
         String [] states = {"TamilNadu","Karnataka","Andhra Pradesh","Maharashtra"};
         ArrayList<String> state = new ArrayList<>(Arrays.asList(states));
         ArrayAdapter<String> arrayAdapterstates = new ArrayAdapter<>(this,support_simple_spinner_dropdown_item,state);
-
+        states_spinner.setAdapter(arrayAdapterstates);
+    }
+    public void openhome()
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/sbdemo/MainActivity.java
+++ app/src/main/java/com/example/sbdemo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.sbdemo;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup = (Button) findViewById(R.id.Signupbutton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opencredentials();
+            }
+        });
+
+    }
+    public void opencredentials()
+    {
+        Intent intent = new Intent(this,Credentials1.class);
+        startActivity(intent);
     }
 }
